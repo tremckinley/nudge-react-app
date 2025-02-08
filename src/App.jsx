f41a332e8bea77abc7fc56de06366d4dffa6cca7
tremckinley <tremckinley901@gmail.com>
2025-02-07 21:44:58 -0600

    added html & css

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,21 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <header>
+      <h1>
+        Nudge's <br />
+        Study Material
+      </h1>
+      <image id="nudge" src="./icons/nudge.png" />
+    </header>
+    <section>
+      <div class="contain">
+        <h2 id="agencyName">Agency</h2>
+        <hr></hr>
+        <p id="agencyDescription">The description of each agency will go here.</p>
+      </div>
+    </section>
+    <script src="sidePanel.js"></script>
     </>
   )
 }
