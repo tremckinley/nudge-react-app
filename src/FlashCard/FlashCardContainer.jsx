17600e93ec85c68823da8dd8acfa1130b3e3807c
Tremckinley <tremckinley901@gmail.com>
2025-04-15 12:31:41 -0500

    refactor: replace icons with buttons in FlashCardContainer for improved accessibility

--- src/FlashCard/FlashCardContainer.jsx
+++ src/FlashCard/FlashCardContainer.jsx
@@ -9,12 +9,12 @@ export default function FlashCardContainer(props) {
 
     return (
         <div className="flex flex-col items-center justify-center my-2">
-            <div className="text-2xl text-[indigo] mb-2">
-                <i className="fa-solid fa-backward-fast" tooltip="Go Back 3" onClick={() => handleLeftClick(true)}></i>
-                <i className="fa-solid fa-backward-step ml-2 mr-4" tooltip="Go Back 1" onClick={() => handleLeftClick()}></i>
-                <i className={"fa-solid mt-2 text-2xl " + (paused ? "text-blue-400 fa-play" : "text-[indigo] fa-pause")} onClick={() => handlePause()}></i>
-                <i className="fa-solid fa-forward-step mr-2 ml-4" onClick={() => handleRightClick()}></i>
-                <i className="fa-solid fa-forward-fast" onClick={() => handleRightClick(true)}></i>
+            <div className="text-2xl text-indigo-900 mb-2">
+                <button className="border p-1 rounded-lg bg-indigo-200"><i className="fa-solid fa-backward-fast" title="Go Back 3" onClick={() => handleLeftClick(true)}></i></button>
+                <button><i className="fa-solid fa-backward-step ml-2 mr-4" title="Go Back 1" onClick={() => handleLeftClick()}></i></button>
+                <button><i className={"fa-solid mt-2 text-2xl " + (paused ? "text-blue-400 fa-play" : "text-[indigo] fa-pause")} onClick={() => handlePause()}></i></button>
+                <button><i className="fa-solid fa-forward-step mr-2 ml-4" onClick={() => handleRightClick()}></i></button>
+                <button><i className="fa-solid fa-forward-fast" onClick={() => handleRightClick(true)}></i></button>
             </div>
             <hr></hr>
             <section className="flex flex-col mx-4 mt-2">
