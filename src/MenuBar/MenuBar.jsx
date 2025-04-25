c5262be5a62409b216bbaa1cbd6ba345abf07bfc
Tremckinley <tremckinley901@gmail.com>
2025-04-25 16:05:25 -0500

    feat: implement SettingsMenu component with clear data functionality and toggle behavior

--- src/MenuBar/MenuBar.jsx
+++ src/MenuBar/MenuBar.jsx
@@ -1,10 +1,17 @@
-export default function MenuBar({props}) {
-    return (
-        <div className='flex w-full items-center h-full'>
-        <h1 className='text-2xl font-bold text-center w-full flex-1'>Nudge's Study Guide</h1>
-        <button onClick={() => {alert('hellow')}} className="hover:cursor-pointer text-indigo-900 text-xl"><i className="fa fa-bars px-2"></i></button>
+import { useState } from "react"
+import SettingsMenu from "./SettingsMenu"
 
-        
+export default function MenuBar({props}) {
+    const [open, setOpen] = useState(false)
+
+    function handleClick() {
+        setOpen(true)
+    }
+    return (
+        <div className='flex w-full items-center h-full relative'>
+            <h1 className='text-2xl font-bold text-center w-full flex-1'>Nudge's Study Guide</h1>
+            <button onClick={handleClick} className="hover:cursor-pointer text-indigo-900 text-xl"><i className="fa fa-bars px-2"></i></button>
+            <SettingsMenu open={open} setOpen={setOpen}/>
         </div>
-    ) 
+    )
 }
