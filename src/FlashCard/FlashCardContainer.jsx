4908bae4c732c4cc50336d1cc6028a793cef9d66
Tremckinley <tremckinley901@gmail.com>
2025-05-02 13:16:56 -0500

    refactor: clean up styles in App.css and layout.css for consistency and readability

--- src/FlashCard/FlashCardContainer.jsx
+++ src/FlashCard/FlashCardContainer.jsx
@@ -9,10 +9,10 @@ export default function FlashCardContainer(props) {
 
     return (
         <div className="flex flex-col items-center justify-center my-2">
-            <div className="text-2xl text-indigo-900 mb-2">
-                <button className="border p-1 rounded-lg bg-indigo-200"><i className="fa-solid fa-backward-fast" title="Go Back 3" onClick={() => handleLeftClick(true)}></i></button>
+            <div className="text-2xl mb-2">
+                <button className="p-1 rounded-lg"><i className="fa-solid fa-backward-fast" title="Go Back 3" onClick={() => handleLeftClick(true)}></i></button>
                 <button><i className="fa-solid fa-backward-step ml-2 mr-4" title="Go Back 1" onClick={() => handleLeftClick()}></i></button>
-                <button><i className={"fa-solid mt-2 text-2xl " + (paused ? "text-blue-400 fa-play" : "text-[indigo] fa-pause")} onClick={() => handlePause()}></i></button>
+                <button><i className={"fa-solid mt-2 text-2xl " + (paused ? "fa-play" : "fa-pause")} onClick={() => handlePause()}></i></button>
                 <button><i className="fa-solid fa-forward-step mr-2 ml-4" onClick={() => handleRightClick()}></i></button>
                 <button><i className="fa-solid fa-forward-fast" onClick={() => handleRightClick(true)}></i></button>
             </div>
